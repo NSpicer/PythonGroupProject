67ea799409e6136ed1a455c79fdc322b85fd94c4
NSpicer <36422885+NSpicer@users.noreply.github.com>
2019-03-31 21:23:09 +0100

    Using Flask

--- number.py
+++ number.py
@@ -1,9 +1,16 @@
 import random
 
+from flask import Flask, render_template, request
+
+app = Flask("Numbers")
+
+@app.route("/randomnumber", methods=["POST"])
+
 def random_numbers():
+	form_data = request.form
 	print("What is the min and max of your random number?")
-	a = int(input("min"))
-	b = int(input("max"))
+	a = int(lowerLimit)
+	b = int(upperLimit)
 
 	number = random.randint(a, b)
 	print number
