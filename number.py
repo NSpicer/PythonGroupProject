af4caca7ceea3c36a5f71cd36f1831a763ee4a20
NSpicer <36422885+NSpicer@users.noreply.github.com>
2019-04-01 11:39:38 +0100

    Trying to get Flash and Jinja2 working

--- number.py
+++ number.py
@@ -4,20 +4,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/templates/NumberGenerator.html", methods=["POST", "GET"])
-def random_numbers():
+@app.route('/minMax', methods=["POST", "GET"])
+def random_number_generate():
 	if request.method=="POST":
 		a = int(request.form['lowerLimit'])
 		b = int(request.form['upperLimit'])
 		number = random.randint(a, b)
-		return redirect(url_for('randomNumber',
+		return redirect(url_for('/NumberResult.html',
 								number=number))
 	return render_template("/NumberGenerator.html")
 
-@app.route("randomNumber", methods = ["GET"])
+@app.route("/NumberGenerator.html", methods = ["GET"])
 def randomNumber():
 		name = request.args.get('number')
-		return render_template("/NumberGenerator.html",
+		return render_template("/NumberResult.html",
 								number=number)
 
 if __name__ =="__main__":
